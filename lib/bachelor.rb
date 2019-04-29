dc68c095d8b2d34e7b7f7902a21315c6ef1b56bb
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 13:53:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,12 @@ require "pry"
 data = JSON.parse(File.read('spec/fixtures/contestants.json'))
 
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].select{|contestants| contestants[:status] == "Winner"}
 end
+
+puts get_first_name_of_season_winner(data, "Season 19")
+puts
+
 
 def get_contestant_name(data, occupation)
   # code here
@@ -23,7 +27,7 @@ end
 
 
 
-bachelor_data.map do |season, contestants|
-  binding.pry
-  [season, contestants.each_with_object([]){|contestant, arr| arr << contestant[:name]}]
-end
+# bachelor_data.map do |season, contestants|
+#   binding.pry
+#   [season, contestants.each_with_object([]){|contestant, arr| arr << contestant[:name]}]
+# end
