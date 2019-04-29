797b96a870745e65096534076b288aff49a46ffe
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 14:45:09 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -65,7 +65,7 @@ puts "Should be Health Club Manager"
 
 def get_average_age_for_season(data, season)
   binding.pry
-  data[season].reduce(0){|sum, contestant| sum + contestant["age"].to_f / contestants.length}
+  data[season].reduce(0){|sum, contestant| sum + contestant["age"].to_f / data[season].length}.to_i
 end
 
 puts
