d84252447ac1a15c3fd766892fb8c608d99a8baf
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 15:18:16 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,26 @@
+# require "pry"
+# data = JSON.parse(File.read('spec/fixtures/contestants.json'))
+
+def contestant_hash(data)
+  data.values.flatten.map{|contestants| [contestants["name"], contestants]}.to_h
+end
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each {|contestant| return contestant["name"].split(" ").first if contestant["status"] == "Winner"}
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  contestant_hash(data).each {|contestant, details| return contestant if details["occupation"] == occupation}
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestant_hash(data).select{|contestant, details| details["hometown"] == hometown}.keys.count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant_hash(data).each{|contestant, details| return details["occupation"] if details["hometown"] == hometown}
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data[season].reduce(0){|sum, contestant| sum + contestant["age"].to_f / data[season].length}.round
 end
