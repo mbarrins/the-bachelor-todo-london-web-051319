49b8650154c398eaf2f923bc72b110ff9b3d3f16
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 13:22:58 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,7 +19,45 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
-batchelor_data.map do |season, contestants|
+
+def batchelor_data
+	{ 
+	  "season 30": [
+	    {
+	      "name":      "Beth Smalls",
+	      "age":       "26",
+	      "hometown":  "Great Falls, Virginia",
+	      "occupation":"Nanny/Freelance Journalist",
+	      "status":    "Winner"
+	    },	
+	    {
+	      "name":       "Becca Tilley",
+	      "age":        "27",
+	      "hometown":   "Shreveport, Louisiana",
+	      "occupation": "Chiropractic Assistant",
+	      "status":     "Eliminated Week 8"
+	    }
+	  ],
+	  "season 29": [
+	    {
+	      "name":      "Ashley Yeats",
+	      "age":       "24",
+	      "hometown":  "Denver, Colorado",
+	      "occupation":"Dental Assitant",
+	      "status":    "Winner"
+	    },
+	    {
+	      "name":       "Sam Grover",
+	      "age":        "29",
+	      "hometown":   "New York, New York",
+	      "occupation": "Entertainer",
+	      "status":     "Eliminated Week 6"
+	    }
+	  ]
+	}
+end
+
+bachelor_data.map do |season, contestants|
   binding.pry
   [season, contestants.each_with_object([]){|arr,contestant| arr << contestant[:name]}]
 end
