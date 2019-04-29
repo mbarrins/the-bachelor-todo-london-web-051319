a3d7450ada9f73604fd12c73a441f9b92ea6a933
Maija Barrins <maija.barrins@gmail.com>
2019-04-29 14:08:47 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -27,5 +27,5 @@ def get_average_age_for_season(data, season)
   # code here
 end
 
-contestant_hash = data.values.flatten.map{|contestants| [contestants[:name], contestants]}.to_h
+contestant_hash = data.values.flatten.map{|contestants| [contestants["name"], contestants]}.to_h
 puts contestant_hash
